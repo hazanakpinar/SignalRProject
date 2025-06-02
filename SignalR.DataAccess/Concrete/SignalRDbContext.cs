de728a987037962ca154ec6bb24f105f99857a33
hazanakpinar <hazanakpinar1@gmail.com>
2025-06-02 10:43:33 +0300

    yeni sınıf eklendi

--- SignalR.DataAccess/Concrete/SignalRDbContext.cs
+++ SignalR.DataAccess/Concrete/SignalRDbContext.cs
@@ -27,6 +27,7 @@ namespace SignalR.DataAccess.Concrete
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<MoneyCase> MoneyCases { get; set; }
+        public DbSet<MenuTable> MenuTables { get; set; }
 
     }
 }
---
+++ SignalR.DataAccess/EntityFramework/efMenuTableDal.cs
@@ -0,0 +1,15 @@
+﻿using SignalR.DataAccess.Abstract;
+using SignalR.DataAccess.Concrete;
+using SignalR.DataAccess.Repositories;
+using SignalR.Entities.Entities;
+
+namespace SignalR.DataAccess.EntityFramework
+{
+    public class efMenuTableDal : GenericRepository<MenuTable>, IMenuTableDal
+    {
+        public efMenuTableDal(SignalRDbContext context) : base(context)
+        {
+        }
+    
+    }
+}
