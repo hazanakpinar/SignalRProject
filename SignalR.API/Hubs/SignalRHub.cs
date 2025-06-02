da4080f4262050c7b45e4ccc77bd225b5569e871
hazanakpinar <hazanakpinar1@gmail.com>
2025-06-02 15:13:55 +0300

    anlık istatistikler ekleniyor

--- SignalR.API/Hubs/SignalRHub.cs
+++ SignalR.API/Hubs/SignalRHub.cs
@@ -1,15 +1,41 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.Business.Abstract;
 using SignalR.DataAccess.Concrete;
 
 namespace SignalR.API.Hubs
 {
     public class SignalRHub:Hub
     {
-        SignalRDbContext context = new SignalRDbContext();
-        public async Task SendCategoryCount()
+        private readonly  ICategoryService _categoryService;
+        private readonly IProductService _productService;
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
         {
-            var value = context.Categories.Count();
-            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+            _categoryService = categoryService;
+            _productService = productService;
         }
+
+        //  SignalRDbContext context = new SignalRDbContext();
+        public async Task SendStatistic()
+        {
+            var value = _categoryService.TActiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+
+            var value2 = _productService.TProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", value2);
+
+            var value3 = _categoryService.TActiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+            var value4 = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("ReceivePasiveCategoryCount", value4);
+
+            var value5 = _productService.TProductCountByCategoryNameHamburger();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameHamburger", value5);
+
+            var value6 = _productService.TProductCountByCategoryNameDrink();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrink",value6);
+        }
+
+  
     }
 }
